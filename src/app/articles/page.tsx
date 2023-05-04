062f668ad683112c3153052ae5e53433c60330f3
swaraj bachu <rajswaraj.r@gmail.com>
2023-05-04 08:12:56 +0530

    trying different technique

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,59 +1,73 @@
 "use client"
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { getBlogs } from '../../../sanity/sanity-utils'
 import { Blog } from '../../../sanity/types/blog';
 import BlogCard from './blog_card';
 import { motion } from 'framer-motion';
 import styles from '../style';
 import { staggerContainer } from '@/utils/motion';
+import { useParams } from 'next/navigation';
 
-export default async function Articles(){
+export default function Articles(){
 
+  const [blogs, setBlogs] = useState<Blog[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
+  useEffect(() => {
+    async function fetchBlogs() {
+      const fetchedBlogs = await getBlogs();
+      setBlogs(fetchedBlogs);
+      setIsLoading(false);
+    }
+    fetchBlogs();
+  }, []);
 
-  const blogs = await getBlogs()
-  console.log(blogs[0])
-
+  if(isLoading) return (
+   <div>
+      <h1>Loading...</h1>
+   </div>
+  );
+      
   return (
     <div className='min-h-screen'>
       <h1 className='font-bold text-3xl mb-7'>Articles</h1>
-      <span  className='absolute gradient-04'/>
+      <span className='absolute gradient-04'/>
 
       <motion.div
-      variants={{
-        hidden: {},
-        show: {
-          transition: {
-            staggerChildren: 0.4,
-            delayChildren: 0.4,
+        variants={{
+          hidden: {},
+          show: {
+            transition: {
+              staggerChildren: 0.4,
+              delayChildren: 0.4,
+            },
           },
-        },
-      }}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: false, amount: 0.25 }}
-      className={`${styles.innerWidth} mx-auto flex flex-col`}
-    >
-      {
-        blogs.map((blog,index) => (
-          console.log(index),
-          <div 
-          key={blog._id}>
-            <BlogCard 
-            index={index}
-            title={blog.title} 
-            description={blog.description} 
-            date={blog.publishedAt} 
-            image={blog.mainImage?.asset.url?blog.mainImage.asset.url:'/placeholder.jpeg'} 
-            alt={blog.mainImage?.alt} 
-            slug={blog.slug.current} 
-            />
-          </div>
-        ))
-      }
-    </motion.div>
+        }}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex flex-col`}
+      >
+        {
+          blogs.map((blog,index) => (
+            <div 
+              key={blog._id}>
+              <BlogCard 
+                index={index}
+                title={blog.title} 
+                description={blog.description} 
+                date={blog.publishedAt} 
+                image={blog.mainImage?.asset.url?blog.mainImage.asset.url:'/placeholder.jpeg'} 
+                alt={blog.mainImage?.alt} 
+                slug={blog.slug.current} 
+              />
+            </div>
+          ))
+        }
+      </motion.div>
     </div>
   );
 
 }
+
